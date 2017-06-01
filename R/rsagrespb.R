11774835aeecb04182585568f3b790d587e16354
Julio Cesar Neves <julio.cesar.silva@ccc.ufcg.edu.br>
2017-06-01 09:41:45 -0300

    Adição de função que retorna valor total de empenhos filtrados para todas as licitacoes

--- R/rsagrespb.R
+++ R/rsagrespb.R
@@ -1,4 +1,6 @@
-# Funções principais para manipulação dos dados do banco do SAGRES-PB
+# Funções principais utilizadas para a manipulação dos dados do banco do SAGRES-PB
+# A menos que declarado o contrário na descrição do método, é sempre necessário coletar os dados retornados por esses métodos
+# antes de utilizá-los. Isso pode ser feito a partir de uma chamada a dplyr::collect()
 
 #' @title get_empenhos_licitacao_filt
 #' @description Retorna os empenhos da licitacao que corresponde aos códigos passados como parâmetro
@@ -28,10 +30,39 @@ get_empenhos_licitacao_filt <- function(dbcon, nu_licitacao, cd_ugestora, tp_lic
   return(empenhos)
 }
 
+
+
+#' @title get_valor_empenhos_licitacoes
+#' @description Retorna o valor total dos empenhos que correspondem aos códigos passados como parâmetro em todas as licitações
+#' @param dbcon conexão com o banco de dados
+#' @param cd_funcao código de função a ser utilizado na filtragem
+#' @param cd_subfuncao código de subfunção a ser utilizado na filtragem
+#' @param cd_subelemento código de subelemento a ser utilizado na filtragem. Valor padrão: 99 (Sem subelemento)
+get_valor_empenhos_licitacoes <- function(dbcon, cd_funcao, cd_subfuncao, cd_subelemento = 99) {
+  query <- sql(paste('
+              select l.cd_ugestora, l.nu_licitacao, l.tp_licitacao, sum(e.vl_empenho) as valor_total_codigo
+              from licitacao l, empenhos e
+              where l.nu_licitacao = e.nu_licitacao
+              and l.cd_ugestora = e.cd_ugestora
+              and l.tp_licitacao = e.tp_licitacao
+              and e.cd_funcao =', cd_funcao,
+            ' and e.cd_subfuncao =', cd_subfuncao,
+            ' and e.cd_subelemento =', cd_subelemento,
+            ' group by l.cd_ugestora, l.nu_licitacao, l.tp_licitacao'
+  ))
+  
+  valor_empenhos <- tbl(dbcon, query)
+  
+  return(valor_empenhos)
+}
+
+
+
 #' @title get_percentual_aditivos
 #' @description Calcula o percentual do valor de aditivos que incide sobre uma licitação. 
 #' Um percentual de exatamente 0 indica que provavelmente o aditivo solicitado para a licitação não foi monetário, mas sim de tempo.
 #' Um retorno 'empty' indica que não há aditivos cadastrados para a licitação analisada.
+#' ATENÇÃO: Não é necessário realizar o collect do retorno deste método.
 #' @param dbcon conexão com o banco de dados
 #' @param nu_licitacao o número da licitação cujo percentual de aditivos deve ser calculado
 #' @param cd_ugestora o código da unidade gestora que lançou a licitação em questão
@@ -43,9 +74,9 @@ get_percentual_aditivos <- function(dbcon, nu_licitacao, cd_ugestora, tp_licitac
                      select a.vl_aditivo/l.vl_licitacao as pr_aditivos 
                      from licitacao l, contratos c, aditivos a 
                      where l.nu_licitacao =', nu_licitacao,
-                     'and l.cd_ugestora =', cd_ugestora,
-                     'and l.tp_licitacao =', tp_licitacao,
-                     'and l.cd_ugestora = c.cd_ugestora 
+                    'and l.cd_ugestora =', cd_ugestora,
+                    'and l.tp_licitacao =', tp_licitacao,
+                    'and l.cd_ugestora = c.cd_ugestora 
                      and l.nu_licitacao = c.nu_licitacao 
                      and l.tp_licitacao = c.tp_licitacao 
                      and c.nu_contrato = a.nu_contrato 
