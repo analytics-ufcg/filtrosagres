3394b77d9e66e00c54193f0bdff062ef25c7808d
Julio Cesar Neves <julio.cesar.silva@ccc.ufcg.edu.br>
2017-06-22 11:10:31 -0300

    Atualiza pacote para utilizar novo SAGRES

--- R/rsagrespb.R
+++ R/rsagrespb.R
@@ -10,13 +10,13 @@
 #' @param tp_licitacao o tipo da licitação em questão
 #' @param cd_funcao código de função a ser utilizado na filtragem
 #' @param cd_subfuncao código de subfunção a ser utilizado na filtragem
-#' @param cd_subelemento código de subelemento a ser utilizado na filtragem. Valor padrão: 99 (Sem subelemento) 
+#' @param cd_subelemento código de subelemento a ser utilizado na filtragem. Valor padrão: 99 (Sem subelemento)
 #' @export
 get_empenhos_licitacao_filt <- function(dbcon, nu_licitacao, cd_ugestora, tp_licitacao,
                                    cd_funcao, cd_subfuncao, cd_subelemento = 99) {
   sql(paste('
               select *
-              from empenhos 
+              from Empenhos
               where nu_licitacao =', nu_licitacao,
             ' and cd_ugestora =', cd_ugestora,
             ' and tp_licitacao =', tp_licitacao,
@@ -24,9 +24,9 @@ get_empenhos_licitacao_filt <- function(dbcon, nu_licitacao, cd_ugestora, tp_lic
             ' and cd_subfuncao =', cd_subfuncao,
             ' and cd_subelemento =', cd_subelemento
             ))
-  
+
   empenhos <- tbl(dbcon, sql)
-  
+
   return(empenhos)
 }
 
@@ -41,7 +41,7 @@ get_empenhos_licitacao_filt <- function(dbcon, nu_licitacao, cd_ugestora, tp_lic
 get_valor_empenhos_licitacoes <- function(dbcon, cd_funcao, cd_subfuncao, cd_subelemento = 99) {
   query <- sql(paste('
               select l.cd_ugestora, l.nu_licitacao, l.tp_licitacao, sum(e.vl_empenho) as valor_total_codigo
-              from licitacao l, empenhos e
+              from Licitacao l, Empenhos e
               where l.nu_licitacao = e.nu_licitacao
               and l.cd_ugestora = e.cd_ugestora
               and l.tp_licitacao = e.tp_licitacao
@@ -50,16 +50,16 @@ get_valor_empenhos_licitacoes <- function(dbcon, cd_funcao, cd_subfuncao, cd_sub
             ' and e.cd_subelemento =', cd_subelemento,
             ' group by l.cd_ugestora, l.nu_licitacao, l.tp_licitacao'
   ))
-  
+
   valor_empenhos <- tbl(dbcon, query)
-  
+
   return(valor_empenhos)
 }
 
 
 
 #' @title get_percentual_aditivos
-#' @description Calcula o percentual do valor de aditivos que incide sobre uma licitação. 
+#' @description Calcula o percentual do valor de aditivos que incide sobre uma licitação.
 #' Um percentual de exatamente 0 indica que provavelmente o aditivo solicitado para a licitação não foi monetário, mas sim de tempo.
 #' Um retorno 'empty' indica que não há aditivos cadastrados para a licitação analisada.
 #' ATENÇÃO: Não é necessário realizar o collect do retorno deste método.
@@ -69,32 +69,32 @@ get_valor_empenhos_licitacoes <- function(dbcon, cd_funcao, cd_subfuncao, cd_sub
 #' @param tp_licitacao o tipo da licitação em questão
 #' @export
 get_percentual_aditivos <- function(dbcon, nu_licitacao, cd_ugestora, tp_licitacao) {
-  
+
   query <- sql(paste('
-                     select a.vl_aditivo/l.vl_licitacao as pr_aditivos 
-                     from licitacao l, contratos c, aditivos a 
+                     select a.vl_aditivo/l.vl_licitacao as pr_aditivos
+                     from Licitacao l, Contratos c, Aditivos a
                      where l.nu_licitacao =', nu_licitacao,
                     'and l.cd_ugestora =', cd_ugestora,
                     'and l.tp_licitacao =', tp_licitacao,
-                    'and l.cd_ugestora = c.cd_ugestora 
-                     and l.nu_licitacao = c.nu_licitacao 
-                     and l.tp_licitacao = c.tp_licitacao 
-                     and c.nu_contrato = a.nu_contrato 
+                    'and l.cd_ugestora = c.cd_ugestora
+                     and l.nu_licitacao = c.nu_licitacao
+                     and l.tp_licitacao = c.tp_licitacao
+                     and c.nu_contrato = a.nu_contrato
                      and c.cd_ugestora = a.cd_ugestora
                      '))
-  
+
   percentual <- tbl(dbcon, query) %>%
     collect()
-  
+
   result <- percentual$pr_aditivos
-  
+
   return(result)
 }
 
 
 
 #' @title get_empenhos_filtrados
-#' @description Filtra os empenhos realizados no estado da Paraíba a partir de seu 
+#' @description Filtra os empenhos realizados no estado da Paraíba a partir de seu
 #' código de função, código de subfunção e código de subelemento.
 #' @param dbcon conexão com o banco de dados
 #' @param cd_funcao código de função a ser utilizado na filtragem
@@ -104,14 +104,14 @@ get_percentual_aditivos <- function(dbcon, nu_licitacao, cd_ugestora, tp_licitac
 get_empenhos_filtrados <- function(dbcon, cd_funcao, cd_subfuncao, cd_subelemento = 99){
   query <- sql(paste('
                      select *
-                     from empenhos
+                     from Empenhos
                      where cd_funcao =', cd_funcao,
-                     'and (cd_subfuncao =', cd_subfuncao, 
+                     'and (cd_subfuncao =', cd_subfuncao,
                      'or cd_subelemento =', cd_subelemento, ')'
   ))
-  
+
   empenhos <- tbl(dbcon, query)
-  
+
   return(empenhos)
 }
 
@@ -119,7 +119,7 @@ get_empenhos_filtrados <- function(dbcon, cd_funcao, cd_subfuncao, cd_subelement
 
 
 #' @title get_pagamentos_filtrados
-#' @description Filtra os pagamentos realizados no estado da Paraíba a partir do 
+#' @description Filtra os pagamentos realizados no estado da Paraíba a partir do
 #' código de função, código de subfunção e código de subelementodos empenhos a que eles estão relacionados.
 #' @param dbcon conexão com o banco de dados
 #' @param cd_funcao código de função a ser utilizado na filtragem
@@ -129,18 +129,18 @@ get_empenhos_filtrados <- function(dbcon, cd_funcao, cd_subfuncao, cd_subelement
 get_pagamentos_filtrados <- function(dbcon, cd_funcao, cd_subfuncao, cd_subelemento = 99){
   query <- sql(paste('
                      select *
-                     from pagamentos
+                     from Pagamentos
                      inner join (
                      select cd_Ugestora, dt_Ano, cd_UnidOrcamentaria, nu_Empenho
-                     from empenhos
+                     from Empenhos
                      where cd_funcao =', cd_funcao,
                      'and (cd_subelemento =', cd_subelemento ,'or cd_subfuncao =', cd_subfuncao, ')
                      ) emm
                      using (cd_Ugestora, dt_Ano, cd_UnidOrcamentaria, nu_Empenho)
                      '))
-  
+
   pagamentos <- tbl(dbcon, query)
-  
+
   return(pagamentos)
 }
 
@@ -148,7 +148,7 @@ get_pagamentos_filtrados <- function(dbcon, cd_funcao, cd_subfuncao, cd_subeleme
 
 
 #' @title get_liquidacoes_filtradas
-#' @description Filtra as liquidações realizadas no estado da Paraíba a partir do 
+#' @description Filtra as liquidações realizadas no estado da Paraíba a partir do
 #' código de função, código de subfunção e código de subelemento dos empenhos a que elas estão relacionadas.
 #' @param dbcon conexão com o banco de dados
 #' @param cd_funcao código de função a ser utilizado na filtragem
@@ -158,18 +158,19 @@ get_pagamentos_filtrados <- function(dbcon, cd_funcao, cd_subfuncao, cd_subeleme
 get_liquidacoes_filtradas <- function(dbcon, cd_funcao, cd_subfuncao, cd_subelemento = 99){
   query <- sql(paste('
       select *
-      from liquidacao
+      from Liquidacao
       inner join (
         select cd_Ugestora, dt_Ano, cd_UnidOrcamentaria, nu_Empenho
-        from empenhos
+        from Empenhos
         where cd_funcao =', cd_funcao,
                      'and (cd_subelemento =', cd_subelemento , 'or cd_subfuncao =', cd_subfuncao, ')
       ) emm
       using (cd_Ugestora, dt_Ano, cd_UnidOrcamentaria, nu_Empenho)
     '))
-  
+
   liquidacoes <- tbl(dbcon, query)
-  
-  
+
+
   return(liquidacoes)
 }
+
